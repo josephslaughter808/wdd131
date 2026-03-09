c1b2731450a6088a232705e8ae82057d391307fd
josephslaughter808 <josephslaughter13@gmail.com>
2026-03-09 14:21:49 -0600

    oush

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// Get current year
+const year = new Date().getFullYear();
+document.getElementById("currentyear").textContent = year;
+
+// Last modified date
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
